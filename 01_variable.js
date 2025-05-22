9087dd037ce85d52d57ce6a66a2914cfa93d982e
YUVRAJ SINGH <singhyuvi4321@gmail.com>
2025-05-22 12:46:03 +0000

    added variable

---
+++ 01_variable.js
@@ -0,0 +1,13 @@
+const accountId = 11100;
+let accountemail = "singhyuvi4321@gmail.com";
+var accountpassword = "2343";
+accountcity = "jaipur";  // allowed, but not recommended without 'let', 'var', or 'const'
+
+// accountId = 2;  // ❌ Error: Assignment to constant variable
+
+accountemail = "shrryuuoap.233@gmail.com";
+accountpassword = "987654";
+
+console.log(accountemail);
+console.table([accountId, accountemail, accountpassword]);
+// prefer to use var
