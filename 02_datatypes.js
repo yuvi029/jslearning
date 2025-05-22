ec3cbc8a71259e140c1115b4215aa9c865cf861f
YUVRAJ SINGH <singhyuvi4321@gmail.com>
2025-05-22 12:46:14 +0000

    added datatypes

---
+++ 02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict";  //treat all js code as newer version\
+// alert(3+3) we are using nodejs not browser
+
+console.log( 3 
+    +
+     3)  //code readabiliiy  b
+console.log("yuvraj")
+let name = "yuvraj"
+let age =20
+let isloggedIn =true/
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean===>true false
+//null===> standalone value
+//indefined==>
+//symbol =>unique
+//object
+console.log(typeof "yuvraj") ///to know the type of data 
+console.log(typeof null);
