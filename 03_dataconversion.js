768dd1fe55a0c9879818dfd921d60252f6ac3414
YUVRAJ SINGH <singhyuvi4321@gmail.com>
2025-05-22 13:28:36 +0000

    dataconversion

---
+++ 03_dataconversion.js
@@ -0,0 +1,37 @@
+ let score = 33
+ console.log(typeof score);
+ console.log(typeof(score));
+ 
+ let valueInNumber =Number(score)
+ console.log(typeof valueInNumber);
+ 
+ console.log(valueInNumber);
+ 
+ let score2= "33ABC"
+ console.log(typeof score2);
+ console.log(typeof(score2));
+ 
+ let valueInNumber1 =Number(score2)
+ console.log(typeof valueInNumber1);
+ 
+ console.log(valueInNumber1);
+ 
+
+ let scor = "TRUE"
+ console.log(typeof scor);
+ console.log(typeof(scor));
+ 
+ let valueInNumber11 =Number(scor)
+ console.log(typeof valueInNumber11);
+ 
+ console.log(valueInNumber11);
+ 
+
+ //"33" ========> 33
+ //"33abc"==> NaN
+ //TRUE=>1;  FALSE====>0
+ 
+ let isLoggedIn = 1
+ let booleanIsloggedin = Boolean(isLoggedIn)
+ console.log(booleanIsloggedin);
+ 
