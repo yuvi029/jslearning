58d5326d6db113b80bb9b4ccd79ccbbc3864aeff
YUVRAJ SINGH <singhyuvi4321@gmail.com>
2025-05-23 15:22:26 +0000

    this deals about stack and heap memories

---
+++ 06_memories.js
@@ -0,0 +1,25 @@
+//heap memories ------for non primitive      //dynamic
+//stack memories---------for primitive           ///lifo
+let myyoutubename="yuvraj is boss";
+let another_name=myyoutubename;
+another_name="sahil";
+console.log(myyoutubename);
+console.log(another_name);
+//here value of myyoutubename doesnot changes but another_name changes as it is stored in stack memory
+
+
+
+let obj1 ={
+    name:'yuvi',
+    phno:91111020200
+}
+let ob = obj1
+
+ob={
+   name: 'singh',
+   phno:  9200020020
+
+}
+console.log(ob);
+//it is heap memory change in one doesnot affect the other
+
